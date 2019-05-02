e18d88ec1ba5cc395c9ab0a686e6a7b6f0d6c0ac
Sean Hagen <sean.r.hagen@gmail.com>
2019-05-02 13:33:01 -0500

    Finished project. Fixed graph to matrix method and table creator method

--- src/SourceCode/TableCreator.java
+++ src/SourceCode/TableCreator.java
@@ -9,123 +9,48 @@ public final class TableCreator {
 
     private String line;
     private int[] biggest;
-    private final long[][] table;
-    private final String title;
-    private final String[] Collumn;
+    private final String[][] table;
     private final int padding = 4;
 
     /**
-     * Creates an asci table when given an array of longs
-     * @param title the title of the asci table
-     * @param table a two dimensional array of type long to build an asci table from
-     * @param Collumn the collumn headers for each collumn in the table
+     * creates and outputs an asci table from a two dimensional string array
+     * @param title
+     * @param table 
      */
-    public TableCreator(String title, long[][] table, String... Collumn) {
-        this.title = title;
+    public TableCreator(String[][] table) {
         this.table = table;
-        this.Collumn = Collumn;
         lineCreator();
-        buildHeader();
         buildLines(table);
 
     }
 
-    /**
-     * this method creates each row of the table when given a two dimensional
-     * array. It checks for special cases such as -1, -2 and -3 that represent
-     * timeout, N/A and out of mem respectively
-     *
-     * @param table a two dimensional table of long values
-     */
+  /**
+   * Takes the two dimensional table and converts it to an asci table.
+   * replaces null values with a space
+   * @param table a two dimensional string table
+   */
     public void buildLines(String[][] table) {
-
+        System.out.println(line);
         for (int x = 0; x < table.length; x++) {
-            String fstring = "|  %" + (biggest[0] - padding) + "d  ";
-            switch ((int) table[x][0]) {
-                case -1:
-                    fstring = "|  %" + (biggest[0] - padding) + "s  ";
-                    System.out.printf(fstring, "TimeOut  ");
-                    break;
-                case -2:
-                    fstring = "|  %" + (biggest[0] - padding) + "s  ";
-                    System.out.printf(fstring, "N/A");
-                    break;
-                case -3:
-                    fstring = "|  %" + (biggest[0] - padding) + "s  ";
-                    System.out.printf(fstring, "Out of Mem");
-                    break;
-                default:
-                    System.out.printf(fstring, table[x][0]);
-                    break;
-            }
-
-            for (int y = 1; y < table[x].length - 1; y++) {
-                fstring = "|%" + (biggest[y] - 2) + "d  ";
-                switch ((int) table[x][y]) {
-                    case -1:
-                        fstring = "|  %" + (biggest[y] - 2) + "s";
-                        System.out.printf(fstring, "TimeOut  ");
-                        break;
-                    case -2:
-                        fstring = "|  %" + (biggest[y] - 2) + "s";
-                        System.out.printf(fstring, "N/A  ");
-                        break;
-                    case -3:
-                        fstring = "|  %" + (biggest[y] - 2) + "s";
-                        System.out.printf(fstring, "Out of Mem  ");
-                        break;
-                    default:
-                        System.out.printf(fstring, table[x][y]);
-                        break;
+            for (int y = 0; y < table[x].length; y++) {
+                //typical format string
+                String fstring = "|%" + (biggest[y] - 2) + "s  ";
+                if (y == table[x].length - 1) {
+                    //if the last cell in the row, add another | to end the row
+                    fstring = "|%" + (biggest[y] - 2) + "s  |\n";
+                }
+                if (x == 0 && y == 0) {
+                    //output an empty cell if null
+                    System.out.printf(fstring, "");
+                } else {
+                    System.out.printf(fstring, table[y][x]);
                 }
 
             }
-            fstring = "|%" + (biggest[biggest.length - 1] - 2) + "d  |\n";
-            switch ((int) table[x][table[x].length - 1]) {
-                case -1:
-                    fstring = "|  %" + (biggest[biggest.length - 1] - 2) + "s|\n";
-                    System.out.printf(fstring, "TimeOut  ");
-                    break;
-                case -2:
-                    fstring = "|  %" + (biggest[biggest.length - 1] - 2) + "s|\n";
-                    System.out.printf(fstring, "N/A  ");
-                    break;
-                case -3:
-                    fstring = "|  %" + (biggest[biggest.length - 1] - 2) + "s|\n";
-                    System.out.printf(fstring, "Out of Mem  ");
-                    break;
-                default:
-                    System.out.printf(fstring, table[x][table[x].length - 1]);
-                    break;
-            }
             System.out.println(line);
-        }
-    }
 
-    /**
-     * creates the header block for the table, including title and collumn
-     * titles uses print format in multiple lines
-     */
-    public void buildHeader() {
-        //prints out the title block above the collumn headers
-        int left = (line.length() / 2 + title.length() / 2);
-        int right = line.length() - left - 2;
-        String fmate = "|%" + left + "s%" + right + "s|\n";
-        System.out.println(line);
-        System.out.printf(fmate, title, "");
-        System.out.println(line);
-        for (int x = 0; x < Collumn.length; x++) {
-            left = (biggest[x] / 2 + Collumn[x].length() / 2);
-            right = biggest[x] - left;
-            if (x == Collumn.length) {
-                fmate = "|%" + left + "s%" + right + "s|\n";
-            } else {
-                fmate = "|%" + left + "s%" + right + "s";
-            }
-            //if it is the last entry, add a bar and a new line character
-            System.out.printf(fmate, Collumn[x], " ");
         }
-        System.out.println("|\n" + line);
+
     }
 
     /**
@@ -152,23 +77,12 @@ public final class TableCreator {
         this.line = line.toString();
     }
 
-    /**
-     * goes through the MapEntryInt and TestNumber arrays to find the longest
-     * value (String Length) in them with padding
-     *
-     * @return an array of int where the first entry is the longest testCount
-     * item, The second is the longest total Collisions count and the third is
-     * the longest max collisions count
-     */
+   /**
+    * finds the biggest entrys in terms of length and adds them to longest collumn entry.
+    */
     private void findTheBiggest() {
         //for each collumn, find the largest entry
-        int[] lengths = new int[Collumn.length];
-        //sets the respective collumns to the length of thier headers
-        //then looks for longer entries in the collumns
-        for (int x = 0; x < lengths.length; x++) {
-            lengths[x] = Collumn[x].length() + padding;
-        }
-
+        int[] lengths = new int[table.length];
         for (int x = 0; x < table.length; x++) {
             for (int y = 0; y < table[x].length; y++) {
                 String temp = table[x][y] + "";
